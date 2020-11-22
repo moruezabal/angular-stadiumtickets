3f0b77aef3862d7dda7fd557f2c76cb4af518abe
Mauricio Oruezabal <moruezabal@gmail.com>
2020-11-22 10:46:38 -0300

    Interface Match creada

---
+++ src/app/match-list/Match.ts
@@ -0,0 +1,9 @@
+export interface Match{
+    match : String;
+    stadium: String;
+    capacity: Number;
+    available: Number;
+    local: String;
+    visitor: String;
+
+}
--- src/app/match-list/match-list.component.ts
+++ src/app/match-list/match-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Match } from './Match';
 
 @Component({
   selector: 'app-match-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatchListComponent implements OnInit {
 
-  match = {
+  match: Match = {
     "match": "Nacional vs Peñarol",
     "stadium" : "Estadio Centenario, Uruguay",
     "capacity" : 60000,
